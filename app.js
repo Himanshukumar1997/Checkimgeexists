adcf70f13065a2f21a9110cb60843e528b9ff5f5
HimanshuKumar1997 <Himanshuk526@gmail.com>
2022-05-09 21:54:39 +0530

    my first commit

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const timer = ms => new Promise(res => setTimeout(res, ms))
 
 const file = reader.readFile(path.join(__dirname ,"./Himashu_project.xlsx"));
   
-let data = []
+let data1 = []
   
 const sheets = file.SheetNames
   
@@ -43,24 +43,47 @@ for(let i = 0; i < sheets.length; i++)
    file.Sheets[file.SheetNames[i]])
 
    for(var j=0;j<temp.length;j++){
-    await timer(1000)
+    await timer(10)
 
-    axios({
-      method: "get",
-      url: temp[j]['Primary Image URL']
-    })
-    .then(response => {
-      if (response.status===200) {
-        data.push(temp[j]['Primary Image URL'])
-        console.log("data is ",data)
-        console.log("response is ",response)
-      } else {
-        console.log('failed');
+    try{
+      const {data}=await axios({
+        method: "get",
+        url: temp[j]['Primary Image URL']
+      })
+  
+      // console.log("status is ",status, " for ",temp[j]['Primary Image URL'])
+
+      if(data){
+        data1.push(temp[j]['Primary Image URL'])
+        console.log("data is ",data1)
+      }else{
+        console.log("Failed to fetch page: ", temp[j]['Primary Image URL']);
       }
-    })
-    .catch(function(err) {
-      console.log("Failed to fetch page: ", temp[j]['Primary Image URL']);
-    })
+    }catch(error){
+      // console.log("error: ", error);
+      if(error?.response?.status===403 && error?.response?.data){
+        data1.push(temp[j]['Primary Image URL'])
+        console.log("data is ",data1)
+      }else{
+        console.log("error is: ",temp[j]['Primary Image URL'])
+      }
+    }
+
+    // axios({
+    //   method: "get",
+    //   url: temp[j]['Primary Image URL']
+    // })
+    // .then(response => {
+    //   if (response.status===200) {
+    //     data.push(response.config.url)
+    //     console.log("data is ",data)
+    //   } else {
+    //     console.log('failed');
+    //   }
+    // })
+    // .catch(function(err) {
+    //   console.log("Failed to fetch page: ", temp[j]['Primary Image URL']);
+    // })
 
    }
 
